a397f8203e3f9a5eff7d8b5adb3252ef374d0086
Vladimir Bondarenko <Vladimirbondarenk0>
2022-04-18 16:58:52 +0300

    added Product catalog

---
+++ constants/catalog.js
@@ -0,0 +1,86 @@
+const CATALOG = [
+    {
+       'id': 'el1',
+        'name': 'Электрогитара HOMAGE HEG300RDS',
+        'img': 'https://i.ibb.co/2qDJxzM/273c9740a5706e48ae010c765e9d4af2.jpg',
+        'price': 17540
+    },
+    {
+        'id': 'el2',
+        'name': 'Гитара Электро-акустическая CORT AD810E-OP',
+       'img': 'https://i.ibb.co/qJXqFcN/3d5ad233adee13087a38473c02e55e17.jpg',
+        'price': 23960
+    },
+    {
+        'id': 'el3',
+        'name': 'Бас-гитара CORT Action-PJ-OPW (коричневая)',
+        'img': 'https://i.ibb.co/WxNWV9f/072848cee4c3de55eaa8e124e53a856f.jpg',
+        'price': 25800
+    },
+    {
+        'id': 'el4',
+        'name': 'Гитара Электро-акустическая HOMAGE LF-4121CEQ',
+        'img': 'https://i.ibb.co/zWM85jC/302d43527f607a638500e47be93c46ec.jpg',
+        'price': 20540
+    },
+    {
+        "id": "el1",
+        "name": "FENDER SQUIER BULLET STRAT HT HSS BLK",
+        "img": "https://i.ibb.co/QJfqs4K/1.jpg",
+        "price": 13600
+    },
+    {
+        "id": "el2",
+        "name": "FENDER SQUIER AFFINITY",
+        "img": "https://i.ibb.co/pKrG5ZJ/2.jpg",
+        "price": 23900
+    },
+    {
+        "id": "el3",
+        "name": "IBANEZ GIO GRG121DX-BKF BLACK FLAT",
+        "img": "https://i.ibb.co/NnvcYhR/3.jpg",
+        "price": 21600
+    },
+    {
+        "id": "el4",
+        "name": "EPIPHONE LES PAUL STUDIO LT EBONY",
+        "img": "https://i.ibb.co/4Tg91WL/4.jpg",
+        "price": 19900
+    },
+    {
+        "id": "el5",
+        "name": "FENDER SQUIER BULLET TREM HSS AWT",
+        "img": "https://i.ibb.co/MC7sy1F/5.jpg",
+        "price": 14500
+    },
+    {
+        "id": "el6",
+        "name": "FENDER SQUIER BULLET MUSTANG HH BLK",
+        "img": "https://i.ibb.co/qNBWPb0/6.jpg",
+        "price": 14600
+    },
+    {
+        "id": "el7",
+        "name": "FENDER SQUIER BULLET STRAT HT AWT",
+        "img": "https://i.ibb.co/WH1h2wV/7.jpg",
+        "price": 13600
+    },
+    {
+        "id": "el8",
+        "name": "JACKSON JS22-7 DINKY SATIN BLACK",
+        "img": "https://i.ibb.co/2cBg9g9/8.jpg",
+        "price": 28700
+    },
+    {
+        "id": "el9",
+        "name": "JACKSON JS11 DINKY OLYMPIC WHITE",
+        "img": "https://i.ibb.co/wKxFRM8/9.jpg",
+        "price": 18100
+    },
+    {
+        "id": "el10",
+        "name": "YAMAHA PACIFICA 012 WH",
+        "img": "https://i.ibb.co/30qJcZX/10.jpg",
+        "price": 15990
+    }
+];
