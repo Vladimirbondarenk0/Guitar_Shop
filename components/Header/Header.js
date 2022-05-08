1c4b226cad8d65b6230e7c8e37eecdd6d749a2b3
Vladimir Bondarenko <Vladimirbondarenk0>
2022-05-08 16:32:27 +0300

    shopping cart component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,15 @@
 class Header {
+    handlerOpenShoppingPage() {
+        shoppingPage.render();
+    }
 
     render(count) {
         const html = `
-        <div class='header-container'>
-            <div class='header-counter'>
-            <img src="img/shopping-cart.png" id='img_shopping-cart'> ${count}
-            </div>
-        </div>
+           <div class="header-container">
+                <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
+                    <img src="img/shopping-cart.png" id='img_shopping-cart'> ${count}
+                </div>
+           </div>
         `;
 
         ROOT_HEADER.innerHTML = html;
@@ -16,5 +19,4 @@ class Header {
 const headerPage = new Header();
 
 const productsStore = localStorageUtil.getProducts();
-
 headerPage.render(productsStore.length);
